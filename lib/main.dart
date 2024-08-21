22f57d9956cbe17fbf8cd297ce1af4566b010584
Mojidul Islam <mojidul31@gmail.com>
2024-08-21 07:26:02 +0600

    statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '/login/login_screen.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,8 +6,46 @@ void main() {
     //theme: ThemeData(
       //primarySwatch: Colors.blue,
     //),
-    home: Scaffold(body: MyApp())
+    home: Scaffold(body: MyApp2())
   ));
+}
+
+class MyApp2 extends StatefulWidget {
+  const MyApp2({super.key});
+
+  @override
+  State<MyApp2> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  String txt = "";
+  @override
+  void initState() {
+    txt = "Statefull Widget";
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(txt),
+              ElevatedButton(
+                  onPressed: () {
+                    print("on press");
+                    setState(() {
+                      txt = "On Press XYZ";
+                    });
+                  },
+                  child: Text("Change")
+              )
+            ],
+          )
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget{
